60371beeeb6c23c06941a3794d96f13f0e66c3aa
Saeid Gholami <saeidxgholami@gmail.com>
2022-02-17 08:02:52 +0330

    Add icons to course card, price field to course model.

--- course/models.py
+++ course/models.py
@@ -5,6 +5,10 @@ from django.utils import timezone
 
 
 class Course(models.Model):
+    PAYMENT_STATUS = (
+        ("free", "Free"),
+        ("paid", "Paid"),
+    )
     image = models.ImageField(upload_to="images/courses", default="course.jpg")
     promotion = models.FileField(upload_to="videos/courses/")
     title = models.CharField(max_length=127)
@@ -13,9 +17,16 @@ class Course(models.Model):
     date_published = models.DateField(null=True, blank=True)
     date_created = models.DateField(auto_now_add=True)
     date_modified = models.DateField(auto_now=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2, default=99.99)
+    payment_status = models.CharField(
+        max_length=4, choices=PAYMENT_STATUS, default="paid"
+    )
 
     def get_absolute_url(self):
         return reverse("course-detail", kwargs={"pk": self.pk})
+
+    def number_of_lessons(self):
+        return sum(len(chapter.lesson_set.all()) for chapter in self.chapter_set.all())
 
     def __str__(self):
         return self.title
